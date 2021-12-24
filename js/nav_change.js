15c5bef341603817327d774b667c2f5bed880071
Sadji0 <60967400+Sadji0@users.noreply.github.com>
2021-12-24 05:31:43 +0300

    fix 4 lab

--- js/nav_change.js
+++ js/nav_change.js
@@ -1,13 +1,12 @@
 window.addEventListener("load", () => {
     let reference = document.location.href.split('/');
+    let queryLinks = document.querySelectorAll('nav a');
 
-    switch(reference[reference.length - 1]){
-        case '':
-        case 'index.html':
-            document.querySelectorAll('nav a')[0].classList.add('active');
-            break;
-        case 'portfolio.html':
-            document.querySelectorAll('nav a')[3].classList.add('active');
-            break;
-    }
+    queryLinks.forEach(elem => {
+        // console.log(elem.className + " " + reference[reference.length - 1]);
+        // console.log(elem.className == reference[reference.length - 1]);
+        if (elem.className === reference[reference.length - 1]) {
+            elem.classList.add('active');
+        }
+    });
 });
